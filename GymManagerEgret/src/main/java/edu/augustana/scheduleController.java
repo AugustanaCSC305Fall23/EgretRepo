52c9e0e2278047bbc83ebc3e85885de04579d816
Stuti Shrestha <stutishrestha21@augustana.edu>
2023-10-10 11:20:07 -0500

    Designed the Plan Maker Schedule View

--- GymManagerEgret/src/main/java/edu/augustana/scheduleController.java
+++ GymManagerEgret/src/main/java/edu/augustana/scheduleController.java
@@ -1,19 +1,39 @@
 package edu.augustana;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class scheduleController {
 
     @FXML
-    private void switchToLibrary() throws IOException {
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button cardsButton;
+
+    @FXML
+    private Button homePage;
+
+    @FXML
+    void switchToLibrary() throws IOException {
         App.setRoot("libraryPage");
     }
 
     @FXML
-    private void openCards() throws IOException {
+    void openCards() throws IOException {
         App.setRoot("PlanMakerCardPage");
     }
 
+    @FXML
+    void initialize() {
+
+    }
 
 }
