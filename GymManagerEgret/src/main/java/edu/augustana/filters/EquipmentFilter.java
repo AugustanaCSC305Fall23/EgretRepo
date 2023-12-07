6d5d3f3f017aaab13032ab85dbaac8c69f1f03ce
Drake Misfeldt <drake.misfeldt@gmail.com>
2023-12-07 01:57:51 -0600

    improved the equipment sorting to use uppercase and split on /

--- GymManagerEgret/src/main/java/edu/augustana/filters/EquipmentFilter.java
+++ GymManagerEgret/src/main/java/edu/augustana/filters/EquipmentFilter.java
@@ -3,16 +3,22 @@ package edu.augustana.filters;
 import edu.augustana.Card;
 import edu.augustana.filters.CardFilter;
 
+import java.util.ArrayList;
+
 public class EquipmentFilter implements CardFilter {
     private String desiredEquipment;
     public EquipmentFilter(String desiredEquipment){
-        this.desiredEquipment = desiredEquipment;
+        this.desiredEquipment = desiredEquipment.toLowerCase();
     }
     public boolean matches(Card candidateCard){
-        if(desiredEquipment.equals("ALL")){
+        if(desiredEquipment.equals("all")){
             return true;
         }else {
-            return candidateCard.getEquipment().contains(desiredEquipment);
+            ArrayList<String> lowerList = new ArrayList<>();
+            for (String item : candidateCard.getEquipment()){
+                lowerList.add(item.toLowerCase());
+            }
+            return lowerList.contains(desiredEquipment);
         }
     }
 }
